1f2c0b6a7c2fedbacdef11cd7ca90193a0a13c43
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2024-10-03 21:25:52 -0300

    modal em js finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,17 +56,17 @@ document.getElementById("btn-acordeon4").addEventListener("click", function () {
 
 //Rolagem Suave no "Inicie Sua Ascensão"
 document.querySelector("#container-infos a").addEventListener("click", function (event) {
-    event.preventDefault(); // Evita o comportamento padrão do link
-    const targetId = this.getAttribute("href"); // Obtém o ID de destino
-    const targetElement = document.querySelector(targetId); // Seleciona o elemento de destino
+  event.preventDefault(); // Evita o comportamento padrão do link
+  const targetId = this.getAttribute("href"); // Obtém o ID de destino
+  const targetElement = document.querySelector(targetId); // Seleciona o elemento de destino
 
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop, // Posição do elemento no topo
-        behavior: "smooth", // Rolagem suave
-      });
-    }
-  });
+  if (targetElement) {
+    window.scrollTo({
+      top: targetElement.offsetTop, // Posição do elemento no topo
+      behavior: "smooth", // Rolagem suave
+    });
+  }
+});
 
 //Hover Suave no "Pronto para sua Ascensão?"
 const alignChamada = document.getElementById("align-chamada");
@@ -99,18 +99,87 @@ document
     }
   });
 
-  //Rolagem Suave no Serviços do Footer
+//Rolagem Suave no Serviços do Footer
 document
-.querySelector("#servicos a")
-.addEventListener("click", function (event) {
-  event.preventDefault();
-  const targetId = this.getAttribute("href");
-  const targetElement = document.querySelector(targetId);
+  .querySelector("#servicos a")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
 
-  if (targetElement) {
-    window.scrollTo({
-      top: targetElement.offsetTop,
-      behavior: "smooth",
-    });
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+
+// Modais Abrir e Fechar
+  
+// Selecionar elementos
+const btnIniciar = document.getElementById('btnIniciar');
+const modalIniciar = document.getElementById('modalIniciar');
+const closeButtons = document.querySelectorAll('.close');
+const btnCadastrar = document.querySelector('.register-text a:nth-of-type(1)'); 
+const btnEsqueceuSenha = document.getElementById('esqueceu-senha'); 
+
+// Função para abrir o modal de iniciar sessão
+function openModalIniciar() {
+  modalIniciar.style.display = 'flex';
+  modalIniciar.style.position = 'fixed';
+}
+
+// Função para fechar todos os modais
+function closeModals() {
+  modalIniciar.style.display = 'none';
+  document.getElementById('modalRegistrar').style.display = 'none';
+  document.getElementById('modalRecuperarSenha').style.display = 'none';
+
+}
+
+// Função para abrir o modal de registro
+function openModalRegistrar() {
+  closeModals(); // Fecha outros modais
+  document.getElementById('modalRegistrar').style.display = 'flex';
+  document.getElementById('modalRegistrar').style.position = 'fixed';
+}
+
+// Função para abrir o modal de recuperação de senha
+function openModalRecuperarSenha() {
+  closeModals(); // Fecha outros modais
+  document.getElementById('modalRecuperarSenha').style.display = 'flex';
+  document.getElementById('modalRecuperarSenha').style.position = 'fixed';
+}
+
+// Eventos de clique
+btnIniciar.addEventListener('click', (event) => {
+  event.preventDefault(); // Previne o comportamento padrão do link
+  openModalIniciar();
+});
+btnCadastrar.addEventListener('click', (event) => {
+  event.preventDefault(); // Previne o comportamento padrão do link
+  openModalRegistrar();
+});
+btnEsqueceuSenha.addEventListener('click', (event) => {
+  event.preventDefault(); // Previne o comportamento padrão do link
+  openModalRecuperarSenha();
+});
+
+// Fechar modais ao clicar no "X"
+closeButtons.forEach(button => {
+  button.addEventListener('click', closeModals);
+});
+
+// Fechar modais ao clicar fora deles
+window.addEventListener('click', (event) => {
+  if (event.target === modalIniciar) {
+    closeModals();
+  }
+  if (event.target === document.getElementById('modalRegistrar')) {
+    closeModals();
+  }
+  if (event.target === document.getElementById('modalRecuperarSenha')) {
+    closeModals();
   }
 });
