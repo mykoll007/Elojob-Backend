b1b3a13406d616030282e6d004cb62faea5e44de
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2024-10-07 21:56:46 -0300

    começando back end

--- bd/banco.sql
+++ bd/banco.sql
@@ -1,11 +1,14 @@
+CREATE SCHEMA `bd_elojob` DEFAULT CHARACTER SET utf8 ;
 
+USE `bd_elojob`;
 
 CREATE TABLE `bd_elojob`.`usuarios` (
-  `id_cadastro` INT NOT NULL AUTO_INCREMENT,
+  `id_cadastro` INT(4) NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
   `telefone` VARCHAR(20) NOT NULL,
+  `token` VARCHAR(255) DEFAULT NULL,
   `data_cadastro` DATETIME NOT NULL,
   PRIMARY KEY (`id_cadastro`))
   ENGINE = InnoDB
